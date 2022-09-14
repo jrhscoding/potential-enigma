466f78e21c7225201409c11caeb254a47b755323
joseph <j.hernandez.92189@gmail.com>
2022-09-14 13:28:31 -0700

    added storage of user input into variable'

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+userArr = [];
+
+const generateLicense = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -16,24 +19,51 @@ const questions = [
     {
         type: 'input',
         name: 'usageInstructions',
-        message: 'Please provide the instructions for use of your project'
+        message: 'Please provide the instructions for installation and use of your project'
     },
     {
         type: 'list',
         name: 'licenseChosen',
         message: 'Please select what license you would like for your project',
-        choices: ['General Public License v3.0 (Does not allow distribution of closed source versions)', 'MIT license (Allows distribution of closed soource versions)', '']
+        choices: ['GPL', 'MIT']
+    },
+    {
+        type: 'input',
+        name: 'contibute',
+        message: 'Please provide instructions on how other developers can contribute to your project'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide examples of how to use any tests you created for your project'
     }
 ];
+// to do!!!++____ insert a table of contents based on information post description
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./README.md', data, fileName, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
 function init() {
 inquirer.prompt(questions)
-.then((answers) => {
-    console.log(answers);
+.then((projectInfo) => {
+    console.log(projectInfo);
+    userArr.push(projectInfo);
+    console.log(userArr[0].projectTitle);
 });
 };
 
