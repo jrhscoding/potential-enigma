d82293f01d147c77cfada77de35ef4e13cb73c04
joseph <j.hernandez.92189@gmail.com>
2022-09-18 14:15:05 -0700

    final updates to markdown generation

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (licenseChosen == 'GPL') {
-
+  if (!data[0].licenseChosen) {
+    return ''
+  } else {
+    console.log(data[0].licenseChosen)
+    return licenseChosen
   }
 }
 
